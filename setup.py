66dccd2c02cddf59364ffb6e701cc893cb80c7e7
Daniel Finger Tavares <danielftavares@gmail.com>
2015-04-15 23:59:15 -0300

    versao para rodar no openshift

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Rest Unisc',
+      version='1.0',
+      description='Aplicacao rest para a cadeira de soa da UNISC',
+      author='Daniel Finger Tavares',
+      author_email='danielftavares@gmail.com',
+      install_requires=['Flask>=0.7.2'],
+     ) 
